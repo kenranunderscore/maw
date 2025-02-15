9be6fd1e90f24f2c31316adfbb3fbafc58c409bd
Johannes Maier <johannes.maier@mailbox.org>
2025-02-15 16:10:33 +0100

    tile existing and new windows horizontally
    Probably lots of wrong behavior, but the most glaring one is that we
    don't handle "disappearing" windows.

--- src-lib/Maw/Main.hs
+++ src-lib/Maw/Main.hs
@@ -1,48 +1,72 @@
 module Maw.Main where
 
+import Control.Monad (forM_)
 import Data.Bits ((.|.))
 import Graphics.X11 qualified as X
-import Graphics.X11.Xlib.Extras (Event (ev_border_width))
 import Graphics.X11.Xlib.Extras qualified as X
 
-eventLoop :: X.Display -> Int -> IO ()
-eventLoop dpy nwins = X.allocaXEvent $ \pe -> do
+defaultWindowChanges :: X.WindowChanges
+defaultWindowChanges = X.WindowChanges 0 0 0 0 0 X.none 0
+
+placeWindow :: X.Display -> X.Window -> (Int, Int) -> IO ()
+placeWindow dpy w (nth, n) = do
+    let height = X.displayHeight dpy (X.defaultScreen dpy)
+    let screenWidth = X.displayWidth dpy (X.defaultScreen dpy)
+    let width :: Float = realToFrac screenWidth / realToFrac n
+    let x = nth * floor width
+    X.moveResizeWindow dpy w (fromIntegral x) 0 (floor width) (fromIntegral height)
+    X.sync dpy False
+    X.flush dpy
+
+eventLoop :: X.Display -> [X.Window] -> IO ()
+eventLoop dpy managedWindows = X.allocaXEvent $ \pe -> do
+    X.sync dpy False
+    X.flush dpy
+    putStrLn "    ping"
     X.nextEvent dpy pe
+    putStrLn "    pong"
     evt <- X.getEvent pe
     print evt
     case evt of
         X.MapRequestEvent{ev_window = w, ev_parent = _p} -> do
-            putStrLn "map requested"
             X.mapWindow dpy w
-            X.sync dpy False
-            X.flush dpy
-            eventLoop dpy nwins
-        X.ConfigureRequestEvent
-            { ev_window = w
-            , ev_value_mask = valueMask
-            , ev_border_width = bw
-            , ev_above = above
-            , ev_detail = detail
-            } -> do
-                putStrLn "configure requested"
-                let height = X.displayHeight dpy (X.defaultScreen dpy)
-                let screenWidth = X.displayWidth dpy (X.defaultScreen dpy)
-                let n = nwins + 1
-                let width :: Float = realToFrac screenWidth / realToFrac n
-                let wc = X.WindowChanges 0 0 (floor width) (fromIntegral height) bw above detail
-                X.moveResizeWindow dpy w 0 0 (floor width) (fromIntegral height)
-                X.configureWindow dpy w valueMask wc
-                X.sync dpy False
-                X.flush dpy
-                eventLoop dpy n
+            eventLoop dpy managedWindows
+        X.ConfigureRequestEvent{ev_window = window} -> do
+            if window `elem` managedWindows
+                then do
+                    putStrLn "  known window, doing nothing"
+                    X.allocaXEvent $ \p -> do
+                        X.setEventType p X.configureNotify
+                        X.setConfigureEvent p window window 0 0 100 150 5 X.none False
+                        X.sendEvent dpy window False X.structureNotifyMask p
+                    eventLoop dpy managedWindows
+                else do
+                    putStrLn "  newly managed window appeared!"
+                    let screenWidth = X.displayWidth dpy (X.defaultScreen dpy)
+                    let windows = window : managedWindows
+                    let newWidth :: Float = realToFrac screenWidth / realToFrac (length windows)
+                    let wc =
+                            defaultWindowChanges
+                                { X.wc_x = 0
+                                , X.wc_y = 0
+                                , X.wc_width = floor newWidth
+                                , X.wc_height = fromIntegral $ X.displayHeight dpy (X.defaultScreen dpy)
+                                }
+                    let mask = X.cWX .|. X.cWY .|. X.cWWidth .|. X.cWHeight
+                    X.configureWindow dpy window (fromIntegral mask) wc
+                    X.sync dpy False
+                    X.flush dpy
+                    forM_ (zip [1 ..] managedWindows) $ \(i, w) -> do
+                        placeWindow dpy w (i, length windows)
+                    eventLoop dpy windows
         _ -> do
-            t <- X.get_EventType pe
-            print $ "unhandled event: " <> show t
+            putStrLn " ..unhandled"
+            eventLoop dpy managedWindows
 
 main :: IO ()
 main = do
     dpy <- X.openDisplay ":1"
     X.selectInput dpy (X.defaultRootWindow dpy) X.substructureRedirectMask
     X.sync dpy False
-    eventLoop dpy 0
+    eventLoop dpy mempty
     X.closeDisplay dpy
